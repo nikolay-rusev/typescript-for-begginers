8091333cc96cc72b42bc646775f8d527a1590a71
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2021-12-31 16:58:40 +0200

    example for Extending types

---
+++ Chapter2/ExtendingTypes/script.ts
@@ -0,0 +1,33 @@
+interface User {
+    name: string;
+    age: number;
+    courses: string[];
+}
+
+interface UserExtended extends User {
+    permissionLevel: string;
+}
+
+const user1: UserExtended = {
+    name: 'Jan',
+    age: 27,
+    courses: ['C', 'C++', 'Python', 'TypeScript'],
+    permissionLevel: 'Instructor',
+};
+
+const user2: UserExtended = {
+    name: 'Daniel',
+    age: 24,
+    courses: ['Java', 'C#'],
+    permissionLevel: 'Student',
+};
+
+function printUser(user: UserExtended) {
+    console.log(user.age);
+    console.log(user.name);
+    console.log(user.courses);
+    console.log(user.permissionLevel);
+}
+
+printUser(user1);
+printUser(user2);
